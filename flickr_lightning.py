983828d849153311e7332563e20e89de27422d4b
JeroendenBoef <jeroendenboef1998@gmail.com>
2021-05-07 14:12:08 +0000

    Bugged lightning

--- flickr_lightning.py
+++ flickr_lightning.py
@@ -28,28 +28,19 @@ from utils import attach_deterministic_distance_embedding
 
 parser = argparse.ArgumentParser(description='Flickr Pytorch Lightning GraphSAINT')
 parser.add_argument('--dropout', type=float, default=0.5)
-parser.add_argument('--batch_size', type=int, default=1000)
-parser.add_argument('--walk_length', type=int, default=3)
 parser.add_argument('--lr', type=float, default=0.01)
-parser.add_argument('--num_steps', type=int, default=5)
-parser.add_argument('--epochs', type=int, default=50)
-parser.add_argument('--num_workers', type=int, default=12)
-parser.add_argument('--runs', type=int, default=20)
-#parser.add_argument('--num_anchor_nodes', type=int, default=0)
-parser.add_argument('--sampling_method', type=str, default='pagerank')
+parser.add_argument('--num_layers', type=int, default=3)
+parser.add_argument('--hidden_layer_size', type=int, default=256)
+parser.add_argument('--batch_size', type=int, default=2048)
+parser.add_argument('--walk_length', type=int, default=3)
+parser.add_argument('--num_steps', type=int, default=30)
+parser.add_argument('--epochs', type=int, default=30)
+parser.add_argument('--num_workers', type=int, default=6)
+#parser.add_argument('--runs', type=int, default=20)
+parser.add_argument('--num_anchor_nodes', type=int, default=0)
+parser.add_argument('--sampling_method', type=str, default='baseline')
 args = parser.parse_args()
 print(args)
-
-# # ensure reproducibility
-# run = 0
-# os.environ['PYTHONHASHSEED'] = str(run)
-# random.seed(run)
-# np.random.seed(run)
-# torch.manual_seed(run)
-# torch.cuda.manual_seed(run)
-# torch.cuda.manual_seed_all(run)
-# torch.backends.cudnn.deterministic = True
-# torch.backends.cudnn.benchmark = False
 
 def seed_worker(worker_id):
     worker_seed = torch.initial_seed() % 2**32
@@ -85,28 +76,35 @@ class Flickr(LightningDataModule):
         self.data = PyGFlickr(self.data_dir)[0]
         row, col = self.data.edge_index
         self.data.edge_weight = 1. / degree(col, self.data.num_nodes)[col]  # Norm by in-degree.
-        self.data.x = attach_deterministic_distance_embedding(data=self.data, num_anchor_nodes=self.num_anchor_nodes, sampling_method=args.sampling_method)
+        #self.data.x = attach_deterministic_distance_embedding(data=self.data, num_anchor_nodes=self.num_anchor_nodes, sampling_method=args.sampling_method)
         train_index, train_weight = subgraph(self.data.train_mask, self.data.edge_index, self.data.edge_weight, relabel_nodes=True) #sample subgraph for graphsaint loader, relabel nodes
         self.train_data = Data(edge_index=train_index, edge_weight=train_weight, x=self.data.x[self.data.train_mask], y=self.data.y[self.data.train_mask])
         
-
+    #uncomment for graphsaint
     def train_dataloader(self):
         return GraphSAINTRandomWalkSampler(self.train_data, batch_size=self.batch_size, walk_length=args.walk_length,
                                     num_steps=args.num_steps, sample_coverage=0,
                                     num_workers=args.num_workers, worker_init_fn=seed_worker,
                                     save_dir=osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Flickr-normalization'))
+    
+    #uncomment for sage
+    # def train_dataloader(self):
+    #     return NeighborSampler(self.data.adj_t, node_idx=self.data.train_mask, sizes=[25, 10], 
+    #                             return_e_id=False, transform=self.convert_batch, 
+    #                             batch_size=self.batch_size, shuffle=True, num_workers=args.num_workers,
+    #                             worker_init_fn=seed_worker, persistent_workers=True)
 
     def val_dataloader(self):
         return NeighborSampler(self.data.adj_t, node_idx=self.data.val_mask, sizes=[25, 10], 
-                                transform=self.convert_batch, batch_size=self.batch_size, 
-                                num_workers=args.num_workers, worker_init_fn=seed_worker, 
-                                persistent_workers=True)
+                                transform=self.convert_batch, return_e_id=False,
+                                batch_size=self.batch_size, num_workers=args.num_workers, 
+                                worker_init_fn=seed_worker, persistent_workers=True)
     
     def test_dataloader(self):
         return NeighborSampler(self.data.adj_t, node_idx=self.data.test_mask, sizes=[25, 10], 
-                                transform=self.convert_batch, batch_size=self.batch_size, 
-                                num_workers=args.num_workers, worker_init_fn=seed_worker, 
-                                persistent_workers=True)
+                                transform=self.convert_batch, return_e_id=False,
+                                batch_size=self.batch_size, num_workers=args.num_workers, 
+                                worker_init_fn=seed_worker, persistent_workers=True)
 
     def convert_batch(self, batch_size, n_id, adjs):
         return Batch(
@@ -119,7 +117,7 @@ class Flickr(LightningDataModule):
 
 class SaintGCN(LightningModule):
     def __init__(self, in_channels: int, out_channels: int,
-                 hidden_channels: int = 256, num_layers: int = 2,
+                 hidden_channels: int, num_layers: int,
                  dropout: float = 0.5):
         super().__init__()
         self.save_hyperparameters()
@@ -130,16 +128,21 @@ class SaintGCN(LightningModule):
         for _ in range(num_layers - 2):
             self.convs.append(SAGEConv(hidden_channels, hidden_channels))
         self.convs.append(SAGEConv(hidden_channels, out_channels))
+
+        self.bns = ModuleList()
+        for _ in range(num_layers - 1):
+            self.bns.append(BatchNorm1d(hidden_channels))
         
-        self.train_acc = Accuracy()
-        self.val_acc = Accuracy()
-        self.test_acc = Accuracy()
+        self.train_acc = Accuracy().clone()
+        self.val_acc = Accuracy().clone()
+        self.test_acc = Accuracy().clone()
 
     def forward(self, x, edge_index, edge_weight=None):
         #training loop on edge index long tensors   
         if type(edge_index) != list: 
-            for conv in self.convs[:-1]:
+            for i, conv in enumerate(self.convs[:-1]):
                 x = conv(x, edge_index, edge_weight)
+                x = self.bns[i](x)
                 x = F.relu(x)
                 x = F.dropout(x, p=self.dropout, training=self.training)
             x = self.convs[-1](x, edge_index, edge_weight)
@@ -149,25 +152,37 @@ class SaintGCN(LightningModule):
             for i, adj_t in enumerate(edge_index):
                 x = self.convs[i]((x, x[:adj_t.size(0)]), adj_t)
                 if i < len(edge_index) - 1:
-                    #x = self.bns[i](x)
+                    x = self.bns[i](x)
                     x = x.relu_()
                     x = F.dropout(x, p=self.dropout, training=self.training)
 
         return x
 
     def training_step(self, batch, batch_idx: int):
+        #uncomment for sage
+        # x, y, adjs_t = batch
+        # y_hat = self(x, adjs_t)
+        # train_loss = F.cross_entropy(y_hat, y)
+        # train_acc = self.train_acc(y_hat.softmax(dim=-1), y)
+        
+        #uncomment for saint
         y_hat = self(batch.x, batch.edge_index)
         train_loss = F.cross_entropy(y_hat, batch.y)
         train_acc = self.train_acc(y_hat.softmax(dim=-1), batch.y)
         self.log('train_acc', train_acc, prog_bar=True, on_step=False,
+                 on_epoch=True)
+        self.log('train_loss', train_loss, prog_bar=True, on_step=False,
                  on_epoch=True)
         return train_loss
 
     def validation_step(self, batch, batch_idx: int):
         x, y, edge_index = batch
         y_hat = self(x, edge_index)
+        val_loss = F.cross_entropy(y_hat, y)
         val_acc = self.val_acc(y_hat.softmax(dim=-1), y)
         self.log('val_acc', val_acc, prog_bar=True, on_step=False,
+                 on_epoch=True)
+        self.log('val_loss', val_loss, prog_bar=True, on_step=False,
                  on_epoch=True)
         return val_acc
 
@@ -180,122 +195,29 @@ class SaintGCN(LightningModule):
         return test_acc
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=0.001)
-
-# def train():
-#     model.train()
-#     model.set_aggr('add' if args.use_normalization else 'mean')
-
-#     total_loss = total_examples = 0
-#     for data in loader:
-#         data = data.to(device)
-#         optimizer.zero_grad()
-
-#         if args.use_normalization:
-#             edge_weight = data.edge_norm * data.edge_weight
-#             out = model(data.x, data.edge_index, edge_weight)
-#             loss = F.nll_loss(out, data.y, reduction='none')
-#             loss = (loss * data.node_norm)[data.train_mask].sum()
-#         else:
-#             out = model(data.x, data.edge_index)
-#             loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
-
-#         loss.backward()
-#         optimizer.step()
-#         total_loss += loss.item() * data.num_nodes
-#         total_examples += data.num_nodes
-#     return total_loss / total_examples
-
-
-# @torch.no_grad()
-# def test():
-#     model.eval()
-#     model.set_aggr('mean')
-
-#     out = model(data.x.to(device), data.edge_index.to(device))
-#     pred = out.argmax(dim=-1)
-#     correct = pred.eq(data.y.to(device))
-
-#     accs = []
-#     for _, mask in data('train_mask', 'val_mask', 'test_mask'):
-#         accs.append(correct[mask].sum().item() / mask.sum().item())
-#     return accs
-
-# logger = Logger(args.runs, args)
-# wandb.init(project=f'GraphPOPE-{args.sampling_method}-{args.num_anchor_nodes}-nodes')
-# config = wandb.config
-# wandb.config.update(args) # adds all of the arguments as config variables
-
-# for run in range(args.runs):
-
-#     # ensure reproducibility
-#     os.environ['PYTHONHASHSEED'] = str(run)
-#     random.seed(run)
-#     np.random.seed(run)
-#     torch.manual_seed(run)
-#     torch.cuda.manual_seed(run)
-#     torch.cuda.manual_seed_all(run)
-#     torch.backends.cudnn.deterministic = True
-#     torch.backends.cudnn.benchmark = False
-
-#     def seed_worker(worker_id):
-#         worker_seed = torch.initial_seed() % 2**32
-#         np.random.seed(worker_seed)
-#         random.seed(worker_seed)
-
-#     print(f'torch seed: {run}')
-    
-#     load_preprocessed_embedding(data=data, num_anchor_nodes=args.num_anchor_nodes, sampling_method=args.sampling_method, run=run) #attach cached embedding
-#     loader = GraphSAINTRandomWalkSampler(data, batch_size=batch_size, walk_length=args.walk_length,
-#                                     num_steps=args.num_steps, sample_coverage=100,
-#                                     num_workers=2, worker_init_fn=seed_worker) 
-    
-#     model = Net(hidden_channels=256).to(device)
-#     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-#     wandb.watch(model) #w&b
-#     for epoch in range(1, 1 + args.epochs):
-#         loss = train()
-#         if epoch % args.log_steps == 0:
-#             print(f'Run: {run + 1:02d}, '
-#                     f'Epoch: {epoch:02d}, '
-#                     f'Loss: {loss:.4f}')
-
-#         if epoch > 9 and epoch % args.eval_steps == 0:
-#             result = test()
-#             logger.add_result(run, result)
-#             train_acc, valid_acc, test_acc = result
-#             print(f'Run: {run + 1:02d}, '
-#                     f'Epoch: {epoch:02d}, '
-#                     f'Train: {100 * train_acc:.2f}%, '
-#                     f'Valid: {100 * valid_acc:.2f}% '
-#                     f'Test: {100 * test_acc:.2f}%')
-#             wandb.log({
-#                 'Train': 100 * train_acc,
-#                 'Valid': 100 * valid_acc,
-#                 'Test': 100 * test_acc
-#                 })
-
-#     logger.add_result(run, result)
-#     logger.print_statistics(run)
-# logger.print_statistics()
+        return torch.optim.Adam(self.parameters(), lr=args.lr)
 
 def main():
-    for num_anchor_nodes in [32, 64, 128, 256]:
-    #num_anchor_nodes=32
-        wandb_logger = WandbLogger(name=f'{args.sampling_method}-{num_anchor_nodes}',project='GraphPOPE-Flickr')
-        seed_everything(42)
-        path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Flickr')
-        datamodule = Flickr(path, batch_size=args.batch_size, num_anchor_nodes=num_anchor_nodes)
-        model = SaintGCN(datamodule.num_features, datamodule.num_classes)
-        checkpoint_callback = ModelCheckpoint(monitor='val_acc', save_top_k=1, dirpath=osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'flickr_checkpoint'))
-        #trainer = Trainer(gpus=[0], max_epochs=10, callbacks=[checkpoint_callback])
+    wandb_logger = WandbLogger(name=f'{args.sampling_method}-{args.num_anchor_nodes}',project='GraphPOPE-Flickr-new')
+    seed_everything(42)
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Flickr')
+    datamodule = Flickr(path, batch_size=args.batch_size, num_anchor_nodes=args.num_anchor_nodes)
+    model = SaintGCN(in_channels=datamodule.num_features, out_channels=datamodule.num_classes, hidden_channels=args.hidden_layer_size, num_layers=args.num_layers)
+    checkpoint_callback = ModelCheckpoint(monitor='train_loss', mode='min', save_top_k=2, dirpath=osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'flickr_checkpoint'))
+    #checkpoint_callback = ModelCheckpoint(monitor='val_acc', save_top_k=1)
+    trainer = Trainer(gpus=1, max_epochs=10, callbacks=[checkpoint_callback, pl.callbacks.early_stopping.EarlyStopping(monitor='train_loss')],
+                        gradient_clip_val=0.5, stochastic_weight_avg=True)
+    # trainer = Trainer(gpus=[0], max_epochs=args.epochs, fast_dev_run=True,
+    #                 callbacks=[checkpoint_callback, pl.callbacks.early_stopping.EarlyStopping(monitor='train_loss')], logger=wandb_logger,
+    #                 gradient_clip_val=0.5, stochastic_weight_avg=True)
 
-        # Uncomment to train on multiple GPUs:
-        trainer = Trainer(gpus=2, accelerator='ddp', max_epochs=50,
-                        callbacks=[checkpoint_callback], logger=wandb_logger)
+    # Uncomment to train on multiple GPUs:
+    # trainer = Trainer(gpus=2, accelerator='ddp', max_epochs=args.epochs,
+    #                 callbacks=[checkpoint_callback, pl.callbacks.early_stopping.EarlyStopping(monitor='train_loss')], logger=wandb_logger,
+    #                 gradient_clip_val=0.5, stochastic_weight_avg=True)
 
-        trainer.fit(model, datamodule=datamodule)
-        trainer.test()
+    trainer.fit(model, datamodule=datamodule)
+    trainer.test(model)
 
 
 if __name__ == "__main__":
--- popequeue.sh
+++ popequeue.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-for param in "pagerank" "degree_centrality"
+for param in "closeness_centrality" "eigenvector_centrality"
 do
+    for num in 32 64 128 256
+    do
+        #echo $param $num
+        python flickr_lightning.py --sampling_method $param --num_anchor_nodes $num
+    done;
     #echo $param
-    python flickr_lightning.py --sampling_method $param
+    #python flickr_lightning.py --sampling_method $param
 done;
 
 # for param in 32 64 128 256
